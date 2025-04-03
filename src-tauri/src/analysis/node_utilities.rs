56aa9848639801e24910f99c98d1ffba4cdaee8d
SauceBoss1 <dtvsworld@gmail.com>
2025-04-03 11:16:59 -0700

    more conversions

--- src-tauri/src/analysis/node_utilities.rs
+++ src-tauri/src/analysis/node_utilities.rs
@@ -1,10 +1,31 @@
+use std::collections::HashMap;
+
 use cp_hierarchical_clustering::DendrogramNode;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct D3Node {
     pub cid: usize,
-    pub name: String,
+    pub name: Option<String>,
     pub dist: f64,
     pub children: Vec<D3Node>,
+}
+
+fn build_d3_tree(root: &DendrogramNode, name_map: &HashMap<usize, String>) -> D3Node {
+    // child array
+    let mut kids: Vec<D3Node> = Vec::new();
+    if let Some(ref left_child) = root.left {
+        kids.push(build_d3_tree(left_child, name_map));
+    }
+
+    if let Some(ref right_child) = root.right {
+        kids.push(build_d3_tree(right_child, name_map));
+    }
+
+    return D3Node {
+        cid: root.cid.clone(),
+        dist: root.distance.clone(),
+        name: name_map.get(&root.cid).cloned(),
+        children: kids,
+    };
 }
