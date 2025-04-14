2e9be071e17d69d3f3815dfe929b73063c101ed5
SauceBoss1 <dtvsworld@gmail.com>
2025-04-14 12:40:37 -0700

    first trial on seeing cluster hierarchy. need to modify visual

--- src/components/ClusterMap.svelte
+++ src/components/ClusterMap.svelte
@@ -5,12 +5,48 @@
 	const tree: string = "/tree.json";
 
 	let d3_tree: any;
+	let svgDiv: HTMLDivElement;
 
 	onMount(async () => {
 		const response = await fetch(tree);
 		const raw = await response.json();
 		console.log(raw);
+
+		d3_tree = raw;
+		drawDendrogram();
 	});
+
+	function drawDendrogram() {
+		if (!d3_tree) return;
+
+		svgDiv.innerHTML = "";
+
+		const width = 600;
+		const height = 1000;
+		const margin = { top: 20, right: 90, bottom: 30, left: 90 };
+
+		const svg = d3
+			.select(svgDiv)
+			.append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom);
+
+		const g = svg
+			.append("g")
+			.attr("transform", `translate(${margin.left},${margin.top})`);
+
+		const root = d3.hierarchy(d3_tree);
+
+		const cluster = d3
+			.cluster()
+			.size([
+				height - margin.top - margin.bottom,
+				width - margin.left - margin.right,
+			]);
+
+		cluster(root);
+	}
 </script>
 
 <h1>Hello World!</h1>
+<div bind:this={svgDiv}></div>
